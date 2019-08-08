0fb9875db4ee2540d8d0f6b7f12aa5ca9d0c51f6
ZQPei <dfzspzq@163.com>
2019-08-08 23:09:10 +0800

    PatchMatch

--- source/main.cpp
+++ source/main.cpp
@@ -41,7 +41,7 @@ int process(const char *image_path, const char *mask_path, const char *output_pa
 
 	cv::Mat ori_image = cv::imread(input_image_path, cv::IMREAD_COLOR);
 	cv::Mat mask_image = cv::imread(input_mask_path, cv::IMREAD_GRAYSCALE);
-	IplImage *ori_ipl_img = &ori_image;
+	IplImage *ori_ipl_img = &IplImage(ori_image);
 
 	assert(ori_image.rows == mask_image.rows && ori_image.cols == mask_image.cols);
 
