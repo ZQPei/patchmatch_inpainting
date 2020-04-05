21a4d499a5632679203268d356ac29a38c0a4d27
zvezdochiot <mykaralw@yandex.ru>
2020-04-05 15:21:41 +0300

    0.2: Added man pages

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ cd ..
 
 
 # run and test on some local cases
-for i in 0 1 2; do
+for i in $(seq 0 2); do
     ./build/patchmatch image_files/inpainting/image/image_0000$i.png \
                        image_files/inpainting/mask/mask_0000$i.png \
                        image_files/inpainting/output/output_0000$i.png \
